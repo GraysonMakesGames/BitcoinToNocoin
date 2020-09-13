ce705455ca993e2d6f55ce74398941507d9e57c5
Viridian <nikostormkilla@gmail.com>
2020-09-13 03:19:32 -0400

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,5 @@
+print("Enter Bitcoin value or amount here.") # Prints the message
+
+print("Bitcoin amount or value is $0 in NoCoin.") # Considering the fact that any currency, cryptocurrency or not, is $0 NoCoin always.
+
+print("NoCoin repo is by Hellogoose. hellogoose/nocoin") # Credits
